3efa7347098353122c47b21edc7c14f688c927f4
Eilon Krauthammer <mitleyber@gmail.com>
2020-07-15 11:32:45 +0300

    Initial Commit

---
+++ BelongHomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BelongHomeProject
+//
+//  Created by Eilon Krauthammer on 15/07/2020.
+//  Copyright © 2020 Eilon Krauthammer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
